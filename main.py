cf71eb55a66c31888a9292ce151a764e6802d8be
shanayacodes <chricelshanaya@gmail.com>
2020-10-27 18:39:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Guess the number!
+
+import random
+
+# Setting the variables
+guess = input('Give me a number between 1 and 20 :)')
+number = random.randint(1,20)
+
+# If the guess isn't the generated number
+while int(guess) != int(number): 
+  if (int(guess) > int(number)):
+   print ('Too high, try again!')
+  elif (int(guess) < int(number)):
+   print ('Too low, guess something higher!')
+  guess = input('Give me a number between 1 and 20 :)')
+
+# If the guess is equal to the number (outside the loop)  
+print ('Yay, you got it! Well done! :D')
+  
+
